4ff3e9a81d8d6733b0c1bbc16ec5f67bbfb7f7f9
Marc Hauschildt <marc.hauschildt@gmail.com>
2025-04-25 00:50:45 -0500

    Commit 8

--- api/server.js
+++ api/server.js
@@ -14,13 +14,30 @@ console.log(typeof JSON.stringify(animal))
 const server = http.createServer(async (req, res) => {
     const destinations = await getDataFromDB()
     if (req.url === '/api' && req.method === 'GET') {
+
         res.setHeader('Content-Type', 'application/json')
         res.statusCode = 200
         res.end(JSON.stringify(destinations))
+
+    } else if (req.url.startsWith('/api/continent') && req.method === 'GET') {
+
+        const continent = req.url.split('/').pop()
+        const filteredData = destinations.filter((destination) => {
+            return destination.continent.toLowerCase() === continent.toLowerCase()
+        })
+        res.setHeader('Content-Type', 'application/json')
+        res.statusCode = 200
+        res.end(JSON.stringify(filteredData))
+
     } else {
+
         res.setHeader('Content-Type', 'application/json')
         res.statusCode = 404
-        res.end(JSON.stringify({error: "not found", message: "The requested route does not exist"}))
+        res.end(JSON.stringify({
+                error: "not found",
+                message: "The requested route does not exist"
+            })
+        )
     }
 })
 
