b27fddddf12847329906c3dcb6d15c1837358dcb
Marc Hauschildt <marc.hauschildt@gmail.com>
2025-04-25 00:20:11 -0500

    Commit 2

--- api/server.js
+++ api/server.js
@@ -3,10 +3,13 @@ import { data } from '../data/data.js'
 import { jsFrameworks } from '../data/jsFrameworks.js'
 const PORT = 8000
 
-const server = http.createServer((req, res) => {
-    // res.end('Hello from the server!')
-    res.setHeader('Content-Type', 'application/json')
-    res.end(JSON.stringify(jsFrameworks))
+const server = http.createServer( (req, res)=> {
+    res.write('This is some data \n')
+    res.write('This is some more data \n')
+    res.end('This is from the server', 'utf8', ()=> console.log('response end'))
 })
 
 server.listen(PORT, () => console.log(`server running on port: ${PORT}`))
+
+// The Network Tool and Related Projects
+// https://scrimba.com/s05e8338en
